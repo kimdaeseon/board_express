4ba76043921b56843392ca57cade1c98cbd2eefc
kimdaeseon <kimdaeseon@khu.ac.kr>
2020-09-07 21:58:46 +0900

    고쳐야지 하고 안고쳤던 파일명 수정

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var session = require('express-session')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/makePost');
 var listRouter = require('./routes/list');
 var postRouter = require('./routes/post');
 
